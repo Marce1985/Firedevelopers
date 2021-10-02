14557a79e2b11e0dc878cc90725ebeb722c3ae8c
AngelicaArrieta <amai8341@gmail.com>
2021-10-02 15:54:57 -0500

    cambio1

--- Dominio/Persona.cs
+++ Dominio/Persona.cs
@@ -7,7 +7,7 @@ namespace Dominio
         public int edad { get; set; }
         public string telefono { get; set; }
         public string direccion { get; set; }
-        
+        public string ciudad { get; set; }
         
     }
 }
