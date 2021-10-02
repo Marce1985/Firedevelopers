32ace27628e24a06af19c759f660e6e2255c7a61
DaniRiveraP <danirivera_92@hotmail.com>
2021-10-02 16:19:56 -0500

    nuevo atributo cliente

--- Dominio/Cliente.cs
+++ Dominio/Cliente.cs
@@ -1,9 +1,10 @@
+using System;
 namespace Dominio
 {
     public class Cliente
     {
         public string id { get; set; }
         public string telefono { get; set; }
-        
+        public String preferencial { get; set;}
     }
 }
